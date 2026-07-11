8acc5b4d39fe14009413b56f738771abfd497318
Hissapp <p42ty7hzy7@privaterelay.appleid.com>
2026-07-11 19:10:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "koudoulog-v4";
+const CACHE = "koudoulog-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
