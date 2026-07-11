e7761713b965aaba34c73b4141be6c3c9e43bbb4
Hissapp <p42ty7hzy7@privaterelay.appleid.com>
2026-07-11 15:39:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,43 @@
-const CACHE = "koudoulog-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
+const CACHE = "koudoulog-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./2048.html",
+  "./sudoku.html",
+  "./sprint.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./icon-512.png",
+  "./icon-2048.png",
+  "./icon-sudoku.png",
+  "./icon-sprint.png"
+];
+
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit ||
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+    )
+  );
 });
